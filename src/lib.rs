961fb46fd046e6d36316e52cdf593de6f171ae56
Vinicius Minotti <vinicius.minotti21@gmail.com>
2024-11-16 22:33:25 -0300

    adding any origin cors

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ pub mod services;
 pub mod models;
 pub mod config;
 
-use actix_web::{web::Data, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http::header, web::Data, App, HttpServer};
 use config::get_database_pool;
 use sqlx::{Pool, Postgres};
 use dotenv::dotenv;
@@ -20,6 +21,13 @@ pub async fn setup_server() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(AppState { db: pool.clone() }))
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+                    .allowed_headers(vec![header::CONTENT_TYPE])
+                    .max_age(3600),
+            )
             .configure(routes::init_routes)
     })
     .bind(("0.0.0.0", 8080))?
--- src/services/qrcode_service.rs
+++ src/services/qrcode_service.rs
@@ -4,6 +4,7 @@ use qrcode::QrCode;
 use sqlx::Error as SqlxError;
 use crate::{models::qrcode_models::{CreateQrCodeBody, GetQrCodeResponse, QrCodeDbResponse}, AppState};
 use base64::{Engine as _, engine::general_purpose};
+use serde_json::json;
 
 pub async fn fetch_qrcodes_logic(state: Data<AppState>) -> Result<HttpResponse, Error> {
     match sqlx::query_as::<_, QrCodeDbResponse>(
@@ -36,11 +37,18 @@ pub async fn create_qrcode_logic(
     state: Data<AppState>,
     body: web::Json<CreateQrCodeBody>,
 ) -> HttpResponse {
-    let qr_code = match QrCode::new(&body.url) {
+    let combined_data = json!({
+        "name": body.name,
+        "description": body.description,
+        "url": body.url,
+        "additional_data": body.additional_data
+    }).to_string();
+    
+    let qr_code = match QrCode::new(&combined_data) {
         Ok(code) => code,
         Err(_) => return HttpResponse::InternalServerError().body("Failed to generate QR code"),
     };
-
+    
     let image = qr_code.render::<Luma<u8>>().build();
     let dynamic_image = DynamicImage::ImageLuma8(image);
 
